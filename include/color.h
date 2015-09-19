47b38a80aa0d595a5deacf0775bfc250da201185
Kengo Tateishi <embrace.ddd.flake.peace@gmail.com>
2015-09-19 23:53:27 +0900

    Filename is shown with bold style

--- include/color.h
+++ include/color.h
@@ -2,11 +2,11 @@
 #define _COLOR_H_
 
 #define ERROR_COLOR "\033[31m"
-#define FILENAME_COLOR "\033[32m"
+#define FILENAME_COLOR "\033[1m\033[32m"
 #define YELLOW_COLOR "\033[33m"
 #define RED_COLOR "\033[31m"
 #define LINE_NO_COLOR "\033[33m"
 #define MATCH_WORD_COLOR "\033[43m\033[30m"
-#define RESET_COLOR "\033[49m\033[39m"
+#define RESET_COLOR "\033[49m\033[39m\033[0m"
 
 #endif // _COLOR_H_
